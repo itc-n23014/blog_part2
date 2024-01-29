407df788d87d7532c299588259de7d2f55b96059
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-29 15:17:43 +0000

    p48 finished
    コンポーネントを作る

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Hero from '../components/hero'
+
 const Home = () => {
   return (
     <>
-      <header>HEADER</header>
-
+      <Header />
       <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
+        <Hero />
       </main>
-
-      <footer>FOOTER</footer>
+      <Footer />
     </>
   )
 }
