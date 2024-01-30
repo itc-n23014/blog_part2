00bf7e35e6b072db8ec4f89f635f8b272ea0df07
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-30 01:55:13 +0000

    p90 finished
    ヒーローのテキストスタイルを設定する

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,10 @@
+import styles from 'styles/hero.module.css'
+
 const Hero = (props = { imageOn: false }) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.subtitle}</p>
+    <div className={styles.text}>
+      <h1 className={styles.title}>{props.title}</h1>
+      <p className={styles.subtitle}>{props.subtitle}</p>
       {props.imageOn && <figure> [画像] </figure>}
     </div>
   )
