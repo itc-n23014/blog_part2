2bae49fbe46e26593bc41229b7d75a37116f378a
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-30 02:37:32 +0000

    p108 finished
    ヒーローのレイアウトを調整

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,11 @@ import styles from 'styles/hero.module.css'
 
 const Hero = (props = { imageOn: false }) => {
   return (
-    <div className={styles.text}>
-      <h1 className={styles.title}>{props.title}</h1>
-      <p className={styles.subtitle}>{props.subtitle}</p>
+    <div className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{props.title}</h1>
+        <p className={styles.subtitle}>{props.subtitle}</p>
+      </div>
       {props.imageOn && <figure> [画像] </figure>}
     </div>
   )
