a4c89943f984562e26771440b32817f3e5d9b6b6
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-30 07:30:22 +0000

    p164 finished
    ヒーローの画像を表示する

--- components/hero.js
+++ components/hero.js
@@ -1,4 +1,6 @@
 import styles from 'styles/hero.module.css'
+import Image from 'next/image'
+import cube from 'images/cube.jpg'
 
 const Hero = (props = { imageOn: false }) => {
   return (
@@ -7,7 +9,19 @@ const Hero = (props = { imageOn: false }) => {
         <h1 className={styles.title}>{props.title}</h1>
         <p className={styles.subtitle}>{props.subtitle}</p>
       </div>
-      {props.imageOn && <figure> [画像] </figure>}
+      {props.imageOn && (
+        <figure className={styles.image}>
+          {' '}
+          <Image
+            src={cube}
+            alt=''
+            layout='responsive'
+            sizes='(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw'
+            priority
+            placeholder='blur'
+          />{' '}
+        </figure>
+      )}
     </div>
   )
 }
