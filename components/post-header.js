69a4b6ef19e6705d88b318b364f0dbdd8690cd31
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-31 12:17:04 +0000

    p230 finished
    投稿日の表記とマークアップ

--- components/post-header.js
+++ components/post-header.js
@@ -1,6 +1,7 @@
 import styles from 'styles/post-header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-regular-svg-icons'
+import ConvertDate from 'components/convert-date'
 
 const PostHeader = (props = { publish: '' }) => {
   return (
@@ -10,7 +11,7 @@ const PostHeader = (props = { publish: '' }) => {
       {props.publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size='ls' color='var(--gray-25)' />
-          {props.publish}
+          <ConvertDate dateISO={props.publish} />
         </div>
       )}
     </div>
