e318c4d2e93e5e510ad04aca7d63b1703358b4e7
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-01-31 05:08:49 +0000

    p212 finished
    非同期処理について

---
+++ pages/blog/schedule.js
@@ -0,0 +1,21 @@
+import { client } from 'lib/api'
+
+const Schedule = () => {
+  return <h1>記事のタイトル</h1>
+}
+export default Schedule
+
+export const getStaticProps = async () => {
+  const resPromise = client.get({
+    endpoint: 'blogs'
+  })
+  try {
+    const res = await resPromise
+    console.log(res)
+  } catch (err) {
+    console.log(err)
+  }
+  return {
+    props: {}
+  }
+}
