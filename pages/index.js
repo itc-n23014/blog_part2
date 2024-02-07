3e39186a006b6989d14336fa956fc98bb5ade136
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-02-07 03:17:28 +0000

    p276 finished
    トップページに記事一覧を表示する

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
+import { getAllPosts } from 'lib/api'
 import Hero from 'components/hero'
 import Container from 'components/container'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 import Meta from 'components/meta'
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
-
 }
 export default Home
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
