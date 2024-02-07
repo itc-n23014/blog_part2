f5c4abbbbbb32faf0a47f080a7917f6ae4659dad
Mitsuhiko Komine <n23014@std.it-college.ac.jp>
2024-02-07 02:31:18 +0000

    p268 finished
    記事一覧を作成

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,25 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' />
       <Hero title='Blog' subtitle='Recent Posts' />
+      <Posts posts={posts} />
     </Container>
   )
 }
 export default Blog
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts()
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
